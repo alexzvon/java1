12432d68d18bfc58ea373bc7117db96f2be57a3c
zvonlexa <zvon.lexa@yandex.ru>
2020-06-02 08:52:47 +0300

    test

---
+++ src/ru/progwards/java1/lessons/test.java
@@ -0,0 +1,7 @@
+package ru.progwards.java1.lessons;
+
+public class test {
+    public static void main(String[] args){
+        System.out.println("Сделаю коммит, запушу в репо: робот, проверяй теперь всё это...");
+    }
+}
