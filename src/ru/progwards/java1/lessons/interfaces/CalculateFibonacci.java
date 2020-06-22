d33f5fe0f452c25889d734c46fc8a7b765adadbc
zvonlexa <zvon.lexa@yandex.ru>
2020-06-22 15:41:02 +0300

    abs7

--- src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
+++ src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
@@ -8,23 +8,28 @@ public class CalculateFibonacci {
     }
 
     public static int fiboNumber(int n) {
-
         if(lastFibo == null) { lastFibo = new CacheInfo(); }
+
         if (n != lastFibo.n) {
 
-            if (n <= 2) return 1;
-
-            int n_2 = 1;
-            int n_1 = 1;
-            int n_s = 0;
-            for (int i = 2; i < n; i++) {
-                n_s = n_1 + n_2;
-                n_1 = n_2;
-                n_2 = n_s;
+            if (n <= 2) {
+                lastFibo.n = n;
+                lastFibo.fibo = 1;
             }
+            else {
+                int n_2 = 1;
+                int n_1 = 1;
+                int n_s = 0;
 
-            lastFibo.n = n;
-            lastFibo.fibo = n_s;
+                for (int i = 2; i < n; i++) {
+                    n_s = n_1 + n_2;
+                    n_1 = n_2;
+                    n_2 = n_s;
+                }
+
+                lastFibo.n = n;
+                lastFibo.fibo = n_s;
+            }
         }
 
         return lastFibo.fibo;
--- src/ru/progwards/java1/lessons/test/Test1.java
+++ src/ru/progwards/java1/lessons/test/Test1.java
@@ -1,8 +1,28 @@
 package ru.progwards.java1.lessons.test;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class Test1 {
     public static void main(String[] args) {
         System.out.print("Сделаю коммит, запушу в репо: робот, проверяй теперь всё это...");
+
+        BigDecimal a = new BigDecimal("10");
+        BigDecimal b = new BigDecimal("20");
+
+        a.multiply(b);
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
     public interface Speaking {
@@ -12,6 +32,14 @@ public class Test1 {
     public interface Eating {
         public String eat();
     }
+
+
+
+
+
+
+
+
 
 
     class Dog implements Speaking, Eating {
