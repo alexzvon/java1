a2c773ee79c8080f7e70d443dc1b56564170f272
zvonlexa <zvon.lexa@yandex.ru>
2020-06-28 15:36:31 +0300

    io2_2

---
+++ src/ru/progwards/java1/lessons/io2/Censor.java
@@ -0,0 +1,68 @@
+package ru.progwards.java1.lessons.io2;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class Censor {
+
+    public static void main(String[] args) {
+        //
+    }
+
+    public static void censorFile(String inoutFileName, String[] obscene) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        long pos;
+
+        try {
+            RandomAccessFile raf = new RandomAccessFile(inoutFileName, "rw");
+            try {
+                for (long l = 0; l < raf.length(); l++) {
+                    int c = raf.read();
+                    if (!Character.isAlphabetic(c)) {
+
+                        for (int i = 0; i < obscene.length; i++) {
+                            if(obscene[i].equals(sb.toString())) {
+                                pos = raf.getFilePointer();
+                                pos -= (long)obscene[i].length() + 1;
+                                raf.seek(pos);
+                                for (int j = 0; j < obscene[i].length(); j++) {
+                                    raf.write(42);
+                                }
+                                pos = raf.getFilePointer();
+                                raf.seek(pos + 1);
+                                break;
+                            }
+                        }
+
+                        sb.delete(0, sb.length());
+                    }
+                    else {
+                        sb.append((char)c);
+                    }
+                }
+            }
+            finally {
+                raf.close();
+            }
+        }
+        catch (IOException exception) {
+            throw new CensorException(exception.getMessage(), inoutFileName);
+        }
+
+    }
+
+    public static class CensorException extends IOException {
+        String mesExc;
+        String fileName;
+
+        public CensorException(String mesExc, String fileName) {
+            this.mesExc = mesExc;
+            this.fileName = fileName;
+        }
+
+        @Override
+        public String toString() {
+            return fileName + ":" + mesExc;
+        }
+    }
+}
