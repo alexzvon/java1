87d2fca4ac2c58cecdccd1281ea87039be78eeec
zvonlexa <zvon.lexa@yandex.ru>
2020-07-13 15:20:09 +0300

    sets-3

--- src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
+++ src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
@@ -126,65 +126,65 @@ public class ProductAnalytics {
     }
 
     public static void main(String[] args) {
-        Product pr1 = new Product("PR1");
-        Product pr2 = new Product("PR2");
-        Product pr3 = new Product("PR3");
-        Product pr4 = new Product("PR4");
-        Product pr5 = new Product("PR5");
-        Product pr6 = new Product("PR6");
-        Product pr7 = new Product("PR7");
-        Product pr8 = new Product("PR8");
-        Product pr9 = new Product("PR9");
-        Product pr0 = new Product("PR0");
-
-        List<Product> products = new ArrayList<>();
-
-        products.add(pr0);
-        products.add(pr1);
-        products.add(pr2);
-        products.add(pr3);
-        products.add(pr4);
-        products.add(pr5);
-        products.add(pr6);
-        products.add(pr7);
-        products.add(pr8);
-        products.add(pr9);
-
-        List<Product> sh_pr0 = new ArrayList<>();
-        List<Product> sh_pr1 = new ArrayList<>();
-        List<Product> sh_pr2 = new ArrayList<>();
-
-        sh_pr0.add(pr1);
-        sh_pr0.add(pr3);
-        sh_pr0.add(pr6);
-        sh_pr0.add(pr8);
-
-        sh_pr1.add(pr2);
-        sh_pr1.add(pr3);
-        sh_pr1.add(pr8);
-
-        sh_pr2.add(pr6);
-        sh_pr2.add(pr7);
-        sh_pr2.add(pr9);
-        sh_pr2.add(pr3);
-        sh_pr2.add(pr4);
-        sh_pr2.add(pr8);
-
-        Shop sh0 = new Shop(sh_pr0);
-        Shop sh1 = new Shop(sh_pr1);
-        Shop sh2 = new Shop(sh_pr2);
-
-        List<Shop> shops = new ArrayList<>();
-
-        shops.add(sh0);
-        shops.add(sh1);
-        shops.add(sh2);
-
-        ProductAnalytics productAnalytics = new ProductAnalytics(products, shops);
-
-        System.out.println(productAnalytics.existInAll());
-        System.out.println(productAnalytics.existAtListInOne());
-        System.out.println(productAnalytics.notExistInShops());
-        System.out.println(productAnalytics.existOnlyInOne());
+//        Product pr1 = new Product("PR1");
+//        Product pr2 = new Product("PR2");
+//        Product pr3 = new Product("PR3");
+//        Product pr4 = new Product("PR4");
+//        Product pr5 = new Product("PR5");
+//        Product pr6 = new Product("PR6");
+//        Product pr7 = new Product("PR7");
+//        Product pr8 = new Product("PR8");
+//        Product pr9 = new Product("PR9");
+//        Product pr0 = new Product("PR0");
+//
+//        List<Product> products = new ArrayList<>();
+//
+//        products.add(pr0);
+//        products.add(pr1);
+//        products.add(pr2);
+//        products.add(pr3);
+//        products.add(pr4);
+//        products.add(pr5);
+//        products.add(pr6);
+//        products.add(pr7);
+//        products.add(pr8);
+//        products.add(pr9);
+//
+//        List<Product> sh_pr0 = new ArrayList<>();
+//        List<Product> sh_pr1 = new ArrayList<>();
+//        List<Product> sh_pr2 = new ArrayList<>();
+//
+//        sh_pr0.add(pr1);
+//        sh_pr0.add(pr3);
+//        sh_pr0.add(pr6);
+//        sh_pr0.add(pr8);
+//
+//        sh_pr1.add(pr2);
+//        sh_pr1.add(pr3);
+//        sh_pr1.add(pr8);
+//
+//        sh_pr2.add(pr6);
+//        sh_pr2.add(pr7);
+//        sh_pr2.add(pr9);
+//        sh_pr2.add(pr3);
+//        sh_pr2.add(pr4);
+//        sh_pr2.add(pr8);
+//
+//        Shop sh0 = new Shop(sh_pr0);
+//        Shop sh1 = new Shop(sh_pr1);
+//        Shop sh2 = new Shop(sh_pr2);
+//
+//        List<Shop> shops = new ArrayList<>();
+//
+//        shops.add(sh0);
+//        shops.add(sh1);
+//        shops.add(sh2);
+//
+//        ProductAnalytics productAnalytics = new ProductAnalytics(products, shops);
+//
+//        System.out.println(productAnalytics.existInAll());
+//        System.out.println(productAnalytics.existAtListInOne());
+//        System.out.println(productAnalytics.notExistInShops());
+//        System.out.println(productAnalytics.existOnlyInOne());
     }
 }
