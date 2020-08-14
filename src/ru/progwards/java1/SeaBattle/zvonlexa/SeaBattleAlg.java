1068da5b22eb5d7b9b523e25f2b08f03d1d24d1a
zvonlexa <zvon.lexa@yandex.ru>
2020-08-14 17:56:02 +0300

    seabattle-4

--- src/ru/progwards/java1/SeaBattle/zvonlexa/SeaBattleAlg.java
+++ src/ru/progwards/java1/SeaBattle/zvonlexa/SeaBattleAlg.java
@@ -63,6 +63,7 @@ public class SeaBattleAlg {
     }
 
     public void battleAlgorithm(SeaBattle seaBattle) {
+        miss = 0;
 
         for (int y = 0; y < seaBattle.getSizeX(); y++) {
             for (int x = 0; x < seaBattle.getSizeY(); x++) {
@@ -90,10 +91,27 @@ public class SeaBattleAlg {
     // функция для отладки
     public static void main(String[] args) {
     	System.out.println("Sea battle");
-    	SeaBattle seaBattle = new SeaBattle(true);
-    	new SeaBattleAlg().battleAlgorithm(seaBattle);
-    	System.out.println(seaBattle.getResult());
+
+
+//    	SeaBattle seaBattle = new SeaBattle(true);
+//    	new SeaBattleAlg().battleAlgorithm(seaBattle);
+//    	System.out.println(seaBattle.getResult());
+
+        test();
+
     }
 
+    public static void test() {
+        SeaBattleAlg alg = new SeaBattleAlg();
+        int result = 0;
+
+        for (int i = 0; i < 1000; i++) {
+            SeaBattle seaBattle = new SeaBattle();
+            alg.battleAlgorithm(seaBattle);
+            result += seaBattle.getResult();
+        }
+
+        System.out.println(result / 1000);
+    }
 }
 
