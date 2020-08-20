c0411a6af95663d0e623713e0262dfb8321e5902
zvonlexa <zvon.lexa@yandex.ru>
2020-08-20 19:58:01 +0300

    file-1

---
+++ src/ru/progwards/java1/lessons/files/FindDuplicates.java
@@ -0,0 +1,138 @@
+package ru.progwards.java1.lessons.files;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+
+public class FindDuplicates {
+
+    public List<List<String>> findDuplicates(String startPath) throws IOException {
+        List<List<String>> result = new ArrayList<>();
+        List<FileDuplicat> lfd;
+
+        for(Map.Entry<Integer, List<FileDuplicat>> entry : listFiles(startPath).entrySet()) {
+            lfd = entry.getValue();
+            if (lfd.size() > 1) {
+                for (FileDuplicat fd: lfd) {
+                    result.add(fd.toList());
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public HashMap<Integer, List<FileDuplicat>> listFiles(String startPath) throws IOException {
+        Path path = Paths.get(startPath);
+        HashMap<Integer, List<FileDuplicat>> hfd = new HashMap<>();
+
+        Files.walkFileTree(path, new SimpleFileVisitor<>(){
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                FileDuplicat fd = new FileDuplicat(file);
+                List<FileDuplicat> lpath;
+                int hC = fd.hashCode();
+
+                if (hfd.containsKey(hC)) {
+                    lpath = hfd.get(hC);
+                }
+                else {
+                    lpath = new ArrayList<>();
+                }
+
+                lpath.add(fd);
+                hfd.put(hC, lpath);
+
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+        });
+
+        return hfd;
+    }
+
+    class FileDuplicat {
+        private Path path;
+
+        public FileDuplicat(Path path) {
+            this.path = path;
+        }
+
+        private int hashContext() {
+            int result = 0;
+
+            try {
+                result = Objects.hash(Files.readString(path));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return result;
+        }
+
+        public List<String> toList() {
+            List<String> ls = new ArrayList<>();
+
+            ls.add(path.getFileName().toString());
+            ls.add(path.getParent().toString());
+
+            return ls;
+        }
+
+        @Override
+        public String toString() {
+            return path.getFileName().toString() + " | " + path.getParent().toString();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(obj == null || getClass() != obj.getClass()) return false;
+            FileDuplicat fd = (FileDuplicat) obj;
+
+            return hashCode() == fd.hashCode();
+        }
+
+        @Override
+        public int hashCode() {
+            String hash = null;
+
+            try {
+                hash = path.getFileName().toString();
+                hash += Files.getAttribute(path, "lastModifiedTime");
+                hash += Files.getAttribute(path, "size");
+                hash += hashContext();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return Objects.hash(hash);
+        }
+    }
+
+    public static void main(String[] args) {
+        String path = "/home/aleksey/leson17";
+
+        FindDuplicates fd = new FindDuplicates();
+
+        try {
+            for (List<String> ls: fd.findDuplicates(path)) {
+                for (String str: ls) {
+                    System.out.print(str);
+                    System.out.print(" | ");
+                }
+                System.out.println("");
+            }
+        }
+        catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
+
---
+++ src/ru/progwards/java1/lessons/test/test17.java
@@ -0,0 +1,67 @@
+package ru.progwards.java1.lessons.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class test17 {
+
+    public static void main(String[] args) {
+
+
+        String name = "tmp865";
+
+        System.out.println(createFolder(name));
+
+        name = "proba.txt";
+
+        System.out.println(replaceF(name));
+
+    }
+
+    public static String createFolder(String name) {
+        Path path = Paths.get(Paths.get("").toAbsolutePath().toString());      //.resolve(name);
+        File file = new File(path.resolve(name).toAbsolutePath().toString());
+
+        file.mkdir();
+
+//        Path path = Paths.get("").resolve(name);
+//        return String.valueOf(path.toAbsolutePath());
+        return path.getParent().toString();
+    }
+
+    public static boolean replaceF(String name) {
+        Path path = Paths.get("").resolve(name);
+
+        try {
+            String context = Files.readString(path);
+            String new_context = "";
+            String c;
+            for (int i = 0; i < context.length(); i++) {
+                c = String.valueOf(context.charAt(i));
+                if(c.equals("F")) {
+                    new_context += "f";
+                }
+                else {
+                    new_context += c;
+                }
+            }
+
+            Files.writeString(path, new_context);
+
+        } catch (IOException e) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+
+//    Реализовать метод с сигнатурой boolean replaceF(String name) который заменяет в файле все F на f,
+//    в случае ошибки вернуть false. Для реализации пользоваться методами java.nio.file.Files.
+
+
+}
