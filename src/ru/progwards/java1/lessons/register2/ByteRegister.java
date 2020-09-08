ddf815fc079d50da2b8a42db1862766eaa64a2dd
zvonlexa <zvon.lexa@yandex.ru>
2020-09-08 11:18:21 +0300

    register2-3

---
+++ src/ru/progwards/java1/lessons/register2/ByteRegister.java
@@ -0,0 +1,42 @@
+package ru.progwards.java1.lessons.register2;
+
+public class ByteRegister extends Register {
+    public Bit[] value = new Bit[8];
+
+    public ByteRegister() {
+        for (int i = 0; i < value.length; i++) {
+            value[i] = new Bit();
+        }
+    }
+
+    public ByteRegister(byte value) {
+        init((int)value);
+    }
+
+    public void init(int value) {
+        for (int i = 0; i < 8; i++) {
+            if (1 == (value & 1)) {
+                this.value[ 7 - i ] = new Bit(true);
+            }
+            else {
+                this.value[ 7 - i ] = new Bit();
+            }
+
+            value >>= 1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String bs = "";
+        for (int i = 0; i < 8; i++) {
+            bs += value[i];
+        }
+
+        return bs;
+    }
+
+    public String toDecString() {
+        return String.valueOf(Integer.parseInt(toString(), 2));
+    }
+}
