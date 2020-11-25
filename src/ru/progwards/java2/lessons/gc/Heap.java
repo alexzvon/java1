944c500b94f263d1462bc30d6a6e9075e70f666d
zvonlexa <zvon.lexa@yandex.ru>
2020-11-25 18:37:50 +0300

    heap-1

---
+++ src/ru/progwards/java2/lessons/gc/Heap.java
@@ -0,0 +1,209 @@
+package ru.progwards.java2.lessons.gc;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Heap {
+    private byte h[];
+    private int mhs;
+
+    public List<Integer> lb = new ArrayList<>();
+    public List<Integer> fb = new ArrayList<>();
+    public HashMap<Integer, Integer> hb = new HashMap<>();
+
+    public Heap(int maxHeapSize) {
+        mhs = maxHeapSize;
+        this.h = new byte[ mhs ];
+        fb.add(0);
+        hb.put(0, mhs);
+    }
+
+    private void copy_block(int in, int to, int size) {
+        for (int i = 0; i < size; i++) {
+            h[ in + i ] = h[ to + i ];
+        }
+    }
+
+    public void compact() {
+        HashMap<Integer, Integer> hb = new HashMap<>();
+        List<Integer> lb = new ArrayList<>();
+        AtomicInteger busy = new AtomicInteger();
+
+        this.lb.stream().sorted(Integer::compareTo).reduce(-1, (o1, o2) -> {
+            if ( o1 == -1 ) {
+                if ( o2 == 0 ) {
+                    int size = this.hb.get(o2);
+                    lb.add(0);
+                    hb.put(0, size);
+                    busy.addAndGet(size);
+                    return o2;
+                }
+                else {
+                    int size = this.hb.get(o2);
+                    copy_block(o2, 0, size);
+                    lb.add(0);
+                    hb.put(0, size);
+                    busy.addAndGet(size);
+                    return 0;
+                }
+            }
+            else {
+                int size_o1 = hb.get(o1);
+                int size_o2 = this.hb.get(o2);
+                int lb_o1 = o1 + size_o1;
+                copy_block(o2, lb_o1, size_o2);
+                lb.add(lb_o1);
+                hb.put(lb_o1, size_o2);
+                busy.addAndGet(size_o2);
+                return lb_o1;
+            }
+        });
+
+        this.hb.clear();
+        this.hb.putAll(hb);
+        this.lb.clear();
+        this.lb.addAll(lb);
+
+        fb.clear();
+        fb.add(busy.get());
+        this.hb.put(busy.get(), mhs - busy.get());
+    }
+
+    public void defrag() {
+        List<Integer> d = new ArrayList<>();
+
+        fb.stream().sorted(Integer::compareTo).reduce((o1, o2) -> {
+            if ( o1 + hb.get(o1) == o2 ) {
+                hb.put(o1, hb.get(o1) + hb.get(o2));
+                hb.remove(o2);
+                d.add(o2);
+                return o1;
+            }
+
+            return o2;
+        });
+
+        fb.removeAll(d);
+    }
+
+    public void free(int ptr) throws InvalidPointerException {
+        for (Integer sb: lb) {
+            if (sb.equals(ptr)) {
+                lb.remove(sb);
+                fb.add(sb);
+                return;
+            }
+        }
+
+        throw new InvalidPointerException("Неверный указатель - " + ptr);
+    }
+
+    public int malloc(int size) throws OutOfMemoryException {
+        boolean compact = true;
+
+        do {
+            Integer result = fb.stream().reduce((o1, o2) -> {
+                int r1 = hb.get(o1) - size;
+                int r2 = hb.get(o2) - size;
+
+                if (r1 < 0) return o2;
+                if (r2 < 0) return o1;
+
+                if (r1 < r2) return o1;
+                else return o2;
+
+            }).orElse(null);
+
+            if ( hb.get(result) != null ) {
+                if (hb.get(result).equals(size)) {
+                    lb.add(result);
+                    fb.remove(result);
+                    return result;
+                }
+
+                if (0 < hb.get(result).compareTo(size)) {
+                    lb.add(result);
+                    fb.remove(result);
+                    int ostIndex = result + size;
+                    fb.add(ostIndex);
+                    hb.put(ostIndex, hb.get(result) - size);
+                    hb.put(result, size);
+                    return result;
+                }
+
+                if (compact && 0 > hb.get(result).compareTo(size)) {
+                    compact();
+                    compact = false;
+                } else {
+                    throw new OutOfMemoryException("Нет свободного блока подходящего размера - " + size);
+                }
+            }
+            else if (compact) {
+                compact();
+                compact = false;
+            }
+            else {
+                throw new OutOfMemoryException("Нет свободного блока подходящего размера - " + size);
+            }
+        } while(true);
+    }
+
+    public static void main(String[] args) {
+        Heap hp = new Heap(200);
+        int r;
+
+        try {
+            r = hp.malloc(20);
+            r = hp.malloc(20);
+            r = hp.malloc(20);
+            r = hp.malloc(20);
+            r = hp.malloc(20);
+            r = hp.malloc(20);
+            r = hp.malloc(20);
+            r = hp.malloc(20);
+            r = hp.malloc(20);
+            r = hp.malloc(20);
+
+            hp.free(20);
+            hp.free(60);
+            hp.free(120);
+//            hp.free(121);
+
+            hp.malloc(30);
+
+            System.out.println(hp.lb);
+            System.out.println("==============================");
+            System.out.println(hp.fb);
+            System.out.println("==============================");
+            System.out.println(hp.hb);
+            System.out.println("==============================");
+            System.out.println("==============================");
+
+            hp.free(0);
+            hp.free(80);
+            hp.free(100);
+
+            System.out.println(hp.lb);
+            System.out.println("==============================");
+            System.out.println(hp.fb);
+            System.out.println("==============================");
+            System.out.println(hp.hb);
+            System.out.println("==============================");
+            System.out.println("==============================");
+
+            hp.defrag();
+
+            System.out.println(hp.lb);
+            System.out.println("==============================");
+            System.out.println(hp.fb);
+            System.out.println("==============================");
+            System.out.println(hp.hb);
+            System.out.println("==============================");
+            System.out.println("==============================");
+
+        }
+        catch (OutOfMemoryException | InvalidPointerException $exception) {
+            System.out.println($exception.getMessage());
+        }
+    }
+}
---
+++ src/ru/progwards/java2/lessons/gc/InvalidPointerException.java
@@ -0,0 +1,7 @@
+package ru.progwards.java2.lessons.gc;
+
+public class InvalidPointerException extends Throwable {
+    public InvalidPointerException(String message) {
+        super(message);
+    }
+}
